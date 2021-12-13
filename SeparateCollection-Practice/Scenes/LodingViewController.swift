076188d6b406b31db0405304c5b8c58a20322c03
LeeHa-Yeon <gkdus1613@naver.com>
2021-12-14 02:13:33 +0900

    [FEAT] 스토리보드 제거 및 초기 셋팅, 카메라 및 사진 권한 info.plist 설정

---
+++ SeparateCollection-Practice/Scenes/LodingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LodingViewController.swift
+//  SeparateCollection-Practice
+//
+//  Created by 이하연 on 2021/12/14.
+//
+
+import UIKit
+
+class LodingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+}
