db254aab75e2afafc4c6f5ea065b76012f4f198f
LeeHa-Yeon <gkdus1613@naver.com>
2021-12-14 14:58:09 +0900

    [FEAT] 로딩화면 구현, JSON 파일 추가

--- SeparateCollection-Practice/Applications/SceneDelegate.swift
+++ SeparateCollection-Practice/Applications/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(windowScene: windowScene)
         window?.backgroundColor = .systemBackground
         
-        window?.rootViewController = CameraViewController()
+        window?.rootViewController = LodingViewController()
         window?.makeKeyAndVisible()
     }
     
--- SeparateCollection-Practice/Scenes/LodingViewController.swift
+++ SeparateCollection-Practice/Scenes/LodingViewController.swift
@@ -6,12 +6,48 @@
 //
 
 import UIKit
+import Lottie
+import Then
+
+
 
 class LodingViewController: UIViewController {
 
+    lazy var animationView = AnimationView(name:"trash1").then {
+        $0.frame = CGRect(x:0, y:0, width: 200, height: 200)
+        $0.center = self.view.center
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        setAnimationView()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playAnimationView()
+    }
+    
+    //MARK: - Functions
+    
+    func setAnimationView(){
+        self.view.addSubview(animationView)
+    }
+    
+    func playAnimationView(){
+        self.animationView.play()
+        self.animationView.loopMode = .loop
+        let time = DispatchTime.now() + .seconds(4)
+        DispatchQueue.main.asyncAfter(deadline: time) {
+            let cameraVC = CameraViewController()
+            cameraVC.modalPresentationStyle = .fullScreen
+            self.present(cameraVC,animated: true,completion: nil)
+        }
     }
 
+
 }
+
