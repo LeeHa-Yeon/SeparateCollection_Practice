5e92fca728a3695b87d80adf4d8fbdf3f549f929
LeeHa-Yeon <gkdus1613@naver.com>
2021-12-15 13:22:01 +0900

    [UPDATE] 배경삭제시 지연시간 발생으로 메시지 알려주기

--- SeparateCollection-Practice/Scenes/PhotoViewController.swift
+++ SeparateCollection-Practice/Scenes/PhotoViewController.swift
@@ -110,6 +110,25 @@ class PhotoViewController: UIViewController {
 
     }
     
+    func showToast(message : String, font: UIFont) {
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = font
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds = true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0 }, completion: {(isCompleted) in
+                toastLabel.removeFromSuperview()
+            }
+        )
+    }
+
+    
     // xBtnPressed
     @objc func xBtnPressed(_ sender: UIButton){
         self.dismiss(animated: true, completion: nil)
@@ -130,6 +149,7 @@ class PhotoViewController: UIViewController {
     }
     
     @objc func removeBgPhoto() {
+        showToast(message: "잠시만 기다려주세요", font: .systemFont(ofSize: 12.0))
         guard let jpgData = self.photoImageView.image?.jpegData(compressionQuality: 0.8) else { return }
         
         AF.upload(
